205e393928ca17d2b650a48f79eb074e6ca39f1d
Lana-018 <140853488+Lana-018@users.noreply.github.com>
2026-06-18 10:33:51 +0300

    Update 01-DataTable-Basics.cs

--- Course-18-C# and Database Connectivity(ADO.NET)/ADO.NET DataTable DataView and DataSet Practice/01-DataTable-Basics.cs
+++ Course-18-C# and Database Connectivity(ADO.NET)/ADO.NET DataTable DataView and DataSet Practice/01-DataTable-Basics.cs
@@ -7,7 +7,7 @@ dt.Columns.Add("Age", typeof(byte));
 dt.Rows.Add(1,"Sama","Isam","26");
 dt.Rows.Add(2, "Hamed", "Abusamaha", "25");
 dt.Rows.Add(3, "Laith", "Rafat", "15");
-dt.Rows.Add(3, "Tamara", "Tarawneh", "30");
+dt.Rows.Add(3, "Tamara", "Tarawneh", "18");
 dt.Rows.Add(3, "Ramzi", "Moqbel", "43");
 
 Console.WriteLine("ID    First Name         Last Name       Age");
@@ -19,3 +19,11 @@ foreach (DataRow row in dt.Rows)
     Console.Write(row["LastName"].ToString().PadRight(17));
     Console.WriteLine(row["Age"].ToString().PadRight(15));
 }
+ Console.WriteLine("__________________________________________");
+ Console.WriteLine("Total persons:" + dt.Rows.Count);
+ Console.WriteLine("Age sum:" + dt.Compute("SUM(Age)", string.Empty));
+ Console.WriteLine("Age average:" + dt.Compute("AVG(Age)", string.Empty));
+ Console.WriteLine("Min age:" + dt.Compute("Min(Age)",string.Empty));
+ Console.WriteLine("Max age:" + dt.Compute("Max(Age)", string.Empty));
+
+            
